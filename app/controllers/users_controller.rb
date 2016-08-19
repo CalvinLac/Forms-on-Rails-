f3c91acc3350e760617ef73e27277752bb9d64a2
Calvin <Calvin.lacc@gmail.com>
2016-08-18 19:10:13 -0600

    Using the form_tag helper, created a simple user sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Congratulations on creating a new account!"
+      redirect_to @user
+    else
+      flash[:error] = "Rats! There were errors when we tried to create your account..."
+      render :new
+    end
+  end
+
+
+  private
+  def user_params
+    params.permit(:username, :password, :email)
   end
 
 end
